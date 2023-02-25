764892f174b9f0280687f79b8d1f1d3228976feb
Abhisek kumar singh <87076425+Denver04@users.noreply.github.com>
2023-02-25 16:57:05 +0530

    search bar added

--- src/componenet/Home/Home.js
+++ src/componenet/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./home.css";
+import SearchIcon from '@mui/icons-material/Search';
 
 function Home() {
   return (
@@ -9,7 +10,12 @@ function Home() {
             <p>loremipsun awua ahvdhaahwv awhvaw dajhdv dajhvdah daj djav dad JG Dd Dddhv hvdhj Dhd HD</p>
         </div>
         <div className='home-button'>
+          <div className='home-input'>
             <input type="search" placeholder='Search your image'></input>
+            <div className='search-icon'>
+              <SearchIcon className='icon' />
+            </div>
+          </div>
             <button type='submit'>Search</button>
         </div>
     </div>
