4aa8f048a873addfb3dd4d45adf69508758ce380
Abhijit-singh04 <2001.abhijitsingh@gmail.com>
2023-02-26 00:07:48 +0530

    changes on hero

--- src/componenet/Home/Home.js
+++ src/componenet/Home/Home.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import "./home.css";
-import SearchIcon from '@mui/icons-material/Search';
+import Hero from '../hero/Hero'
+import background from '../../background.jpg'
 
 function Home() {
-  return (
+  return (<>
+
     <div className='home'>
-        <div className='home-head'>
-            <h4>Get the <span>right images</span> you want</h4>
-            <p>loremipsun awua ahvdhaahwv awhvaw dajhdv dajhvdah daj djav dad JG Dd Dddhv hvdhj Dhd HD</p>
-        </div>
-        <div className='home-button'>
-          <div className='home-input'>
-            <input type="search" placeholder='Search your image'></input>
-            <div className='search-icon'>
-              <SearchIcon className='icon' />
-            </div>
-          </div>
-            <button type='submit'>Search</button>
-        </div>
+
+      <img className='home__image' src={background} alt="" />
+      <div className='home__container'>
+        <Hero />
+      </div>
     </div>
+
+  </>
   )
 }
 
